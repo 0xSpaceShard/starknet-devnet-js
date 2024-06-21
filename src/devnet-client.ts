78aa9fa7313c88f674c53237744b4f9646d25f7e
FabijanC <fabijan.corak@gmail.com>
2024-06-21 17:11:22 +0200

    Add mint

--- src/devnet-client.ts
+++ src/devnet-client.ts
@@ -8,6 +8,16 @@ export type DevnetClientConfig = {
     url?: string;
 };
 
+export type BigNumberish = string | number | bigint;
+
+export type BalanceUnit = "WEI" | "FRI";
+
+export type MintResponse = {
+    new_balance: bigint;
+    unit: BalanceUnit;
+    tx_hash: string;
+};
+
 export class DevnetClient {
     public url: string;
     private httpClient: AxiosInstance;
@@ -21,12 +31,22 @@ export class DevnetClient {
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    private async sendRpc(method: string, params: any) {
-        return this.httpClient.post(RPC_URL, {
+    private async sendRpc(method: string, params: any): Promise<any> {
+        const resp = await this.httpClient.post(RPC_URL, {
             jsonrpc: "2.0",
             id: "1",
             method,
             params,
+        });
+
+        return new Promise((resolve, reject) => {
+            if ("result" in resp.data) {
+                resolve(resp.data["result"]);
+            } else if ("error" in resp.data) {
+                reject(resp.data["error"]);
+            } else {
+                reject(resp.data);
+            }
         });
     }
 
@@ -39,5 +59,23 @@ export class DevnetClient {
                 })
                 .catch(() => resolve(false));
         });
+    }
+
+    public async mint(
+        address: string,
+        amount: BigNumberish,
+        unit: BalanceUnit = "WEI",
+    ): Promise<MintResponse> {
+        const respData = await this.sendRpc("devnet_mint", {
+            address,
+            amount: amount.toString(), // stringify to ensure serializability
+            unit,
+        });
+
+        return {
+            new_balance: BigInt(respData.new_balance),
+            unit: respData.unit,
+            tx_hash: respData.tx_hash,
+        };
     }
 }
--- test/devnet-client.test.ts
+++ test/devnet-client.test.ts
@@ -1,10 +1,27 @@
 import { expect } from "chai";
-import { DevnetClient } from "../dist/devnet-client";
+import { DevnetClient, BalanceUnit } from "../src/devnet-client";
 
-describe("Client", function () {
+describe("DevnetClient", function () {
     it("should have a healthcheck endpoint", async function () {
         const devnetClient = new DevnetClient();
-        const status = await devnetClient.isAlive();
-        expect(status).to.be.true;
+        const isAlive = await devnetClient.isAlive();
+        expect(isAlive).to.be.true;
+    });
+
+    it("can mint", async function () {
+        const devnetClient = new DevnetClient();
+        const dummyAddress = "0x1";
+        const dummyAmount = 20n;
+        for (const unit of ["WEI" as BalanceUnit, "FRI" as BalanceUnit]) {
+            const mintRespData = await devnetClient.mint(dummyAddress, dummyAmount, unit);
+            expect(mintRespData.tx_hash).to.match(/^0x[0-9a-fA-F]+/);
+            expect(mintRespData.new_balance).to.equal(dummyAmount);
+            expect(mintRespData.unit).to.equal(unit);
+        }
+
+        const defaultUnitMintRespData = await devnetClient.mint(dummyAddress, dummyAmount);
+        expect(defaultUnitMintRespData.tx_hash).to.match(/^0x[0-9a-fA-F]+/);
+        expect(defaultUnitMintRespData.new_balance).to.equal(dummyAmount);
+        expect(defaultUnitMintRespData.unit).to.equal("WEI");
     });
 });
