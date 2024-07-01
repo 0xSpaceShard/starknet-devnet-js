03b8b6477c67f1095ac051eb6a565a4c6848dba5
FabijanC <fabijan.corak@gmail.com>
2024-07-01 13:19:07 +0200

    Publish automatically if version incremented on master

---
+++ scripts/npm-publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+LOCAL_VERSION=$(jq -r ".version" package.json)
+NPM_VERSION=$(npm view @starknet-devnet dist-tags.latest)
+
+if [ $LOCAL_VERSION = $NPM_VERSION ]; then
+    echo "Latest npm version is equal to current package version. Up the version to publish to npm."
+else
+    npm ci
+    npm run build
+    # NPM access token: https://docs.npmjs.com/creating-and-viewing-access-tokens
+    npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
+    npm publish --verbose --access=public
+fi
