9cd8a278ef5f7c6af46fee93d344c437c5727455
FabijanC <fabijan.corak@gmail.com>
2024-09-09 17:00:58 +0200

    Add debug line

--- test/account-impersonation.test.ts
+++ test/account-impersonation.test.ts
@@ -58,6 +58,7 @@ describe("Account impersonation", function () {
             await invocation;
             assert.fail("Invocation should have failed");
         } catch (err) {
+            console.log("DEBUG err", err);
             const typedErr = err as LibraryError;
             expect(typedErr.message).to.contain("Account validation failed");
         }
